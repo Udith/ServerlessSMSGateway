2040fe4a995394acaad44b09d15feb7bf896a0d5
Udith Gunaratna <uditharosha@gmail.com>
2018-03-26 01:05:07 +0530

    Added SNS SMS code

--- SMSGateway/lambda.js
+++ SMSGateway/lambda.js
@@ -1,5 +1,6 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
 	let receiver = event['receiver'];
 	let sender = event['sender'];
@@ -7,5 +8,26 @@ exports.handler = function(event, context, callback) {
 
 	console.log("Sending message", message, "to receiver", receiver);
 
-	callback(null,'Successfully executed');
+	sns.publish({
+		Message: message,
+		MessageAttributes: {
+			'AWS.SNS.SMS.SMSType': {
+				DataType: 'String',
+				StringValue: 'Promotional'
+			},
+			'AWS.SNS.SMS.SenderID': {
+				DataType: 'String',
+				StringValue: sender
+			},
+		},
+		PhoneNumber: receiver
+	}).promise()
+		.then(data => {
+			console.log("Sent message to", receiver);
+			callback(null, data);
+		})
+		.catch(err => {
+			console.log("Sending failed", err);
+			callback(null);
+		});
 }
